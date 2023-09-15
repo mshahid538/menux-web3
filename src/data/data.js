be839e9e30de5cc1283e5de26622f93deaf634c8
khawarqureshi <khawarqureshi08@gmail.com>
2023-09-15 16:44:41 +0500

    move deitary and allergy data from constant to data.js file

--- src/data/data.js
+++ src/data/data.js
@@ -2,6 +2,23 @@ import r1 from "../assets/img/r1.jpg";
 import r2 from "../assets/img/r2.jpg";
 import r3 from "../assets/img/r3.jpg";
 
+// allergy icons
+import nuts from "../assets/img/nuts.png";
+import Vegan from "../assets/img/Vegan.png";
+import penuts from "../assets/img/penuts.png";
+import fish from "../assets/img/fish.png";
+import eggs from "../assets/img/eggs.png";
+import milk from "../assets/img/milk.png";
+import soya from "../assets/img/soya.png";
+import chery from "../assets/img/chery.png";
+import mustard from "../assets/img/mustard.png";
+import lupin from "../assets/img/lupin.png";
+import seeds from "../assets/img/seed.png";
+import sulphate from "../assets/img/sulphate.png";
+import gluten from "../assets/img/gluten.png";
+import m from "../assets/img/m.png";
+import c from "../assets/img/c.png";
+
 export const Data = {
   restaurants: [
     {
@@ -259,3 +276,95 @@ export const Data = {
     gluten: "Gluten",
   },
 };
+
+// allergy data
+export const AllergicList = [
+  {
+    name: "Nuts",
+    icon: nuts,
+  },
+  {
+    name: "Peanuts",
+    icon: penuts,
+  },
+  {
+    name: "Fish",
+    icon: fish,
+  },
+  {
+    name: "Eggs",
+    icon: eggs,
+  },
+  {
+    name: "Crustaceans",
+    icon: c,
+  },
+  {
+    name: "Molluscs",
+    icon: m,
+  },
+  {
+    name: "Milk",
+    icon: milk,
+  },
+  {
+    name: "Soya",
+    icon: soya,
+  },
+  {
+    name: "Celery",
+    icon: chery,
+  },
+  {
+    name: "Mustard",
+    icon: mustard,
+  },
+  {
+    name: "Lupin",
+    icon: lupin,
+  },
+  {
+    name: "Sesame Seeds",
+    icon: seeds,
+  },
+  {
+    name: "Sulphite",
+    icon: sulphate,
+  },
+  {
+    name: "Cereals w Gluten",
+    icon: gluten,
+  },
+];
+
+// Dietary requirements data
+export const DietaryReq = [
+  {
+    id: 1,
+    name: "Lactose Intolerant",
+  },
+  {
+    id: 2,
+    name: "Gluten Intolerant/Sensitive",
+  },
+  {
+    id: 3,
+    name: "Vegetarian",
+  },
+  {
+    id: 4,
+    name: "Vegan",
+  },
+  {
+    id: 5,
+    name: "Kosher",
+  },
+  {
+    id: 6,
+    name: "Halal",
+  },
+  {
+    id: 7,
+    name: "Diabetic",
+  },
+];
