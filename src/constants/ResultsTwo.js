a9e870702e3bebd8d839566cdda909810b90931c
khawarqureshi <khawarqureshi08@gmail.com>
2023-09-18 14:28:25 +0500

    drinks added with products

--- src/constants/ResultsTwo.js
+++ src/constants/ResultsTwo.js
@@ -40,13 +40,65 @@ export const ResultsTwo = [
         id: 5,
         name: "Fanta",
       },
+      {
+        id: 6,
+        name: "Appletiser",
+      },
     ],
   },
   {
     name: "Belvoir",
+    products: [
+      {
+        id: 1,
+        name: "Belvoir Lemonade",
+      },
+      {
+        id: 2,
+        name: "Belvoir Raspberry Lemonade",
+      },
+      {
+        id: 3,
+        name: "Belvoir Elderflower and Rose",
+      },
+      {
+        id: 4,
+        name: "Belvoir Elderflower",
+      },
+      {
+        id: 5,
+        name: "Belvoir Ginger Beer",
+      },
+    ],
   },
   {
     name: "Juices",
+    products: [
+      {
+        id: 1,
+        name: "Apple",
+      },
+      {
+        id: 2,
+        name: "Cranberry",
+      },
+      {
+        id: 3,
+        name: "Orange",
+      },
+      {
+        id: 4,
+        name: "Pineapple",
+      },
+      {
+        id: 5,
+        name: "Pomegranate",
+      },
+      {
+        id: 6,
+        name: "Mango",
+      },
+    ],
   },
   {
     name: "Hot Drinks",
@@ -57,23 +109,45 @@ export const ResultsTwo = [
       },
       {
         id: 2,
-        name: "Capinccino",
+        name: "Cappuccino",
       },
       {
         id: 3,
-        name: "Capinccino",
+        name: "Latte",
       },
       {
         id: 4,
-        name: "Doubl Expresses",
+        name: "Espresso",
       },
       {
         id: 5,
+        name: "Double Espresso",
+      },
+      {
+        id: 6,
+        name: "Macchiato",
+      },
+      {
+        id: 7,
         name: "Hot Chocolate",
       },
     ],
   },
   {
     name: "Teas",
+    products: [
+      {
+        id: 1,
+        name: "Green",
+      },
+      {
+        id: 2,
+        name: "English Breakfast",
+      },
+      {
+        id: 3,
+        name: "Mint",
+      },
+    ],
   },
 ];
