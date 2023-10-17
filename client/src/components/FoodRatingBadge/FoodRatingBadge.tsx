65f1430c4e71236f35367113c162d8fd909fb093
khawarqureshi <khawarqureshi08@gmail.com>
2023-10-17 17:24:35 +0500

    img changes and new tab

--- client/src/components/FoodRatingBadge/FoodRatingBadge.tsx
+++ client/src/components/FoodRatingBadge/FoodRatingBadge.tsx
@@ -1,27 +1,36 @@
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 
 const FoodRatingBadge = () => {
   useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://ratings.food.gov.uk/embed/embed-badge.js";
-    script.async = true;
-    script.setAttribute("data-business-id", "1273442");
-    script.setAttribute("data-rating-style", "3");
-    script.setAttribute("data-welsh", "false");
+    const link = document.createElement("a");
+    link.href =
+      "https://ratings.food.gov.uk/business/1273442/ribeye-steakhouse-manchester/online-ratings";
+    link.target = "_blank";
+    link.rel = "noopener noreferrer";
+
+    const img = document.createElement("img");
+    img.src =
+      "https://ratings.food.gov.uk/embed/badges/fhrs/3/fhrs-badge-5.svg";
+    img.alt = "Food hygiene rating is 5: Very good";
+    img.style.minWidth = "291px";
+    img.style.height = "120px";
+    img.style.width = "200px";
+
+    link.appendChild(img);
 
     const foodRatingContainer = document.getElementById("foodRatingContainer");
     if (foodRatingContainer) {
-      foodRatingContainer.appendChild(script);
+      foodRatingContainer.appendChild(link);
     }
 
     return () => {
       if (foodRatingContainer) {
-        foodRatingContainer.removeChild(script);
+        foodRatingContainer.removeChild(link);
       }
     };
   }, []);
 
-  return <div id="foodRatingContainer"></div>;
+  return <div id="foodRatingContainer">{}</div>;
 };
 
 export default FoodRatingBadge;
